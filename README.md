4497ea596c60317ffbd5b4f05ccc16bc4466116b
Egor Kovetskiy <e.kovetskiy@gmail.com>
2018-02-24 17:42:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# log-go
+
+The logger based on lorg and structured-logger-go, main goal of that package is
+decreasing amount of spaghetti code in microservices.
---
+++ log.go
@@ -0,0 +1,84 @@
+package log
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	structured "github.com/MagalixTechnologies/structured-logger-go"
+	"github.com/kovetskiy/lorg"
+)
+
+type Logger struct {
+	*structured.Logger
+}
+
+func New(debug bool, trace bool, traceFile string) *Logger {
+	stderr := lorg.NewLog()
+	stderr.SetIndentLines(true)
+	stderr.SetFormat(
+		lorg.NewFormat("${time} ${level:[%s]:right:short} ${prefix}%s"),
+	)
+
+	stderr.Infof("trace log file: %s", traceFile)
+
+	logfile, err := os.OpenFile(
+		traceFile,
+		os.O_WRONLY|os.O_CREATE|os.O_APPEND,
+		0666,
+	)
+	if err != nil {
+		stderr.Fatalf(
+			"unable to create log file: %s", err,
+		)
+	}
+
+	if logfile.Name() != os.Stderr.Name() {
+		output := lorg.NewOutput(logfile)
+
+		output.SetLevelWriterCondition(lorg.LevelTrace, logfile)
+		output.SetLevelWriterCondition(lorg.LevelDebug, logfile)
+		output.SetLevelWriterCondition(lorg.LevelFatal, logfile, os.Stderr)
+		output.SetLevelWriterCondition(lorg.LevelError, logfile, os.Stderr)
+		output.SetLevelWriterCondition(lorg.LevelWarning, logfile, os.Stderr)
+		output.SetLevelWriterCondition(lorg.LevelInfo, logfile, os.Stderr)
+
+		stderr.SetOutput(output)
+	}
+
+	if debug {
+		stderr.SetLevel(lorg.LevelDebug)
+	}
+
+	if trace {
+		stderr.SetLevel(lorg.LevelTrace)
+	}
+
+	return &Logger{structured.NewLogger(stderr)}
+}
+
+func (logger *Logger) TraceJSON(obj interface{}) (encoded string) {
+	if logger.GetLevel() != lorg.LevelTrace {
+		return ""
+	}
+
+	defer func() {
+		err := recover()
+		if err != nil {
+			encoded = fmt.Sprintf(
+				"%#v (unable to encode to json: %s)",
+				obj, err,
+			)
+		}
+	}()
+
+	contents, err := json.MarshalIndent(obj, "", " ")
+	if err != nil {
+		return fmt.Sprintf(
+			"%#v (unable to encode to json: %s)",
+			obj, err,
+		)
+	}
+
+	return string(contents)
+}
